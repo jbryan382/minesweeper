0969094b7318de50db11685035937b491f73c572
Jordan Bryan <jbryan382@gmail.com>
2019-02-25 14:14:50 -0500

    Set up basic structure of minesweeper

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Mine from './components/Mine'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <main>
+          <section>
+            <h1>Smelly Bombs!</h1>
+            <button className="reset">Reset</button>
+          </section>
+          <section>
+            <Mine />
+          </section>
+        </main>
+      </>
+    )
   }
 }
 
---
+++ src/components/Mine.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Mine extends Component {
+  render() {
+    return (
+      <>
+        <button />
+      </>
+    )
+  }
+}
+
+export default Mine
