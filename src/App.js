6fc3a57b1a718f7e3ec52748322c3a56929c5fbf
Jordan Bryan <jbryan382@gmail.com>
2019-02-25 16:07:15 -0500

    Added Dynamic Minefield and added depth with css.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react'
-import Mine from './components/Mine'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    game: [[]]
+  }
+
+  componentDidMount() {
+    axios
+      .post('https://minesweeper-api.herokuapp.com/games', { difficulty: 0 })
+      .then(resp => {
+        this.setState({
+          game: resp.data.board
+        })
+      })
+  }
+
   render() {
     return (
-      <>
-        <main>
-          <section>
-            <h1>Smelly Bombs!</h1>
-            <button className="reset">Reset</button>
-          </section>
-          <section>
-            <Mine />
-          </section>
-        </main>
-      </>
+      <main>
+        <section>
+          <h1>💣 Smelly Bombs! 💣</h1>
+          <button className="reset">Reset</button>
+        </section>
+        <section className="game_body">
+          <table>
+            <tbody>
+              {this.state.game.map(row => {
+                return (
+                  <tr>
+                    {row.map(col => {
+                      // Put an onClick{} event on td for functionality.
+                      return <td>{col}</td>
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </section>
+      </main>
     )
   }
 }
