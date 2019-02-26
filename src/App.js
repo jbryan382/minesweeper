0394ae9d35bf4aa0a46adfbc1538d7ec5d78d374
Jordan Bryan <jbryan382@gmail.com>
2019-02-26 16:33:59 -0500

    Added game api, flags, and reset button.

--- src/App.js
+++ src/App.js
@@ -11,22 +11,23 @@ class App extends Component {
     mines: 0
   }
 
-  setDifficulty = event => {
-    console.log(event)
-    if (
-      event.target.value === 'Intermediate' &&
-      event.target.value !== 'Beginner'
-    ) {
-      this.setState({ difficulty: 1 })
-    } else {
-      this.setState({ difficulty: 2 })
-    }
-  }
+  // setDifficulty = event => {
+  //   console.log(event)
+  //   if (
+  //     event.target.value === 'Intermediate' &&
+  //     event.target.value !== 'Beginner'
+  //   ) {
+  //     this.setState({ difficulty: 1 })
+  //   } else {
+  //     this.setState({ difficulty: 2 })
+  //   }
+  // }
   componentDidMount() {
     axios
-      .post('https://minesweeper-api.herokuapp.com/games', { setDifficulty() })
+      .post('https://minesweeper-api.herokuapp.com/games', { difficulty: 0 })
       .then(resp => {
         this.setState({
+          id: resp.data.id,
           game: resp.data.board,
           mines: resp.data.mines
         })
@@ -34,7 +35,58 @@ class App extends Component {
       })
   }
 
-  // Call to API everytime the gameboard if clicked to update the gmae. 
+  startGame = (x, y) => {
+    axios
+      .post(
+        `https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`,
+        {
+          id: this.state.id,
+          row: x,
+          col: y,
+          mines: 0
+        }
+      )
+      .then(resp => {
+        console.log(resp)
+        this.setState({
+          game: resp.data.board
+        })
+      })
+  }
+
+  resetGame = () => {
+    axios
+      .post('https://minesweeper-api.herokuapp.com/games', { difficulty: 0 })
+      .then(resp => {
+        this.setState({
+          id: resp.data.id,
+          game: resp.data.board,
+          mines: resp.data.mines
+        })
+        console.log(this.state.mines)
+      })
+  }
+
+  plantFlag = (x, y) => {
+    axios
+      .post(
+        `https://minesweeper-api.herokuapp.com/games/${this.state.id}/flag`,
+        {
+          id: this.state.id,
+          row: x,
+          col: y,
+          mines: 0
+        }
+      )
+      .then(resp => {
+        console.log(resp)
+        this.setState({
+          game: resp.data.board
+        })
+      })
+  }
+
+  // Call to API every time the game board if clicked to update the game.
 
   render() {
     return (
@@ -44,20 +96,29 @@ class App extends Component {
           <select>
             <option>Beginner</option>
             {/* onChange or onSelect */}
-            <option onSelect={this.setDifficulty}>Intermediate</option>
-            <option onSelect={this.setDifficulty}>Expert</option>
+            {/* <option onSelect={this.setDifficulty}>Intermediate</option> */}
+            {/* <option onSelect={this.setDifficulty}>Expert</option> */}
           </select>
-          <button className="reset">Reset</button>
+          <button className="reset" onClick={() => this.resetGame()}>
+            😀
+          </button>
         </section>
         <section className="game_body">
           <table>
             <tbody>
-              {this.state.game.map(row => {
+              {this.state.game.map((row, x) => {
                 return (
-                  <tr>
-                    {row.map(col => {
-                      // Put an onClick{} event on td for functionality. Open Context Menu
-                      return <td>{col}</td>
+                  <tr key={x}>
+                    {row.map((col, y) => {
+                      return (
+                        <td
+                          key={y}
+                          onClick={() => this.startGame(x, y)}
+                          onContextMenu={() => this.plantFlag(x, y)}
+                        >
+                          {col}
+                        </td>
+                      )
                     })}
                   </tr>
                 )
